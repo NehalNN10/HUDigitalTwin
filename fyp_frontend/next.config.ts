0b08b6d2ea16e5e4a6e1f22d66ffa59553f3c535
NehalNN10 <nh07884@st.habib.edu.pk>
2026-05-14 15:49:39 +0500

    Update configuration file

--- fyp_frontend/next.config.ts
+++ fyp_frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: 'https://pretended-surgery-likely.ngrok-free.dev/api/:path*', // Proxies all API calls to Flask
+        destination: "https://pretended-surgery-likely.ngrok-free.dev/api/:path*", // Proxies all API calls to Flask
       },
     ];
   },
